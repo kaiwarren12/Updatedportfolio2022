801d9109c55227a01e77219ea99caa70fae54fec
Kai Warren <kaiwarren12@yahoo.com>
2022-03-26 04:20:15 -0700

    fixed errors in header.js due to typos

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -9,17 +9,70 @@ export default function Header () {
     const [selectedScreen, setSelectedScreen] = useState(0)
     const [showHeaderOptions, setShowHeaderOptions] = useState(false)
 
-    const updateCurrentScreen = (currentscreen)=>{
+    const updateCurrentScreen = (currentScreen)=>{
         if(!currentScreen || !currentScreen.screenInView) 
         return;
-        let screenIndex = GET_SCREEN_INDEX(currentSreen.screenInView)
+        let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView)
         if(screenIndex < 0)
         return
     }
     let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen)
+    
+    const getHeaderOptions = ()=>{
+        return (
+            TOTAL_SCREENS.map((screen, i)=>(
+                <div key={Screen.screen_name} className={getOptionsClass(i)}
+                onClick={() => switchScreen(i, screen)}>
+                    <span>{Screen.screen_name}</span>
+                </div>
+            ))
+        )
+    }
+    const getHeaderOptionsClass =(index)=>{
+        let classes = "header-option";
+        if (index < TOTAL_SCREENS.length -1)
+        classes += "header-option-seperator";
+
+        if(selectedScreen === index)
+        classes += "selected-header-option";
+        return
+    }
+
+    const switchScreen = (index, screen) =>{
+        let screenComponent = document.getElementById(screen.screen_name)
+        if(!screenComponent)
+        return
+
+        screenComponent.scrollIntoView({behavior: "smooth"});
+        setSelectedScreen(index);
+        setShowHeaderOptions(false);
+    };
+
+    useEffect (()=>{ 
+        return () => {
+            currentScreenSubscirption.unsubscribe();
+        };
+    }, [currentScreenSubscription]);
+    
     return (
         <div>
+                <div className = "header-options" onClick={()=>setShowHeaderOptions(!showHeaderOptions)}>
+                    <div className='header-parent'>
+                        <div className="header-hamburger" onClick={()=>setShowHeaderOptions(!showHeaderOptions)}>
 
+                        <FontAwesomeIcon className='header-hamburger-bars' icon={faBars} />
+                    </div>
+                    <div className="header-logo">
+                        <span>KAI~</span>
+                    </div>
+                    <div className={(showHeaderOptions)?"header-options show-hamburger-options": "header-options"}>
+                        {getHeaderOptions()}
+                    </div>
+                )
+            )
+         </div> 
         </div>
+     )
+    </div>
     )
-}
+};
