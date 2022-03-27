767e98e6381774d1f1d83f41013627944b55fc99
Kai Warren <kaiwarren12@yahoo.com>
2022-03-26 23:34:30 -0700

    fixed issues with about me alignment

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -4,12 +4,12 @@ import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import './Home.css';
 
-export default function Home () {
+export default function Home (props) {
     return (
-        <div className="home-container">
+        <div className="home-container" id={props.id || ""}>
             <Header/>
             <Profile/>
             <Footer/>
         </div>
-    )
+    );
 }
